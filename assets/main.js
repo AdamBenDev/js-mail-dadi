4e371b644cd895c020cafc810cc56153ca800861
AdamBenDev <adamaya9732@gmail.com>
2023-03-14 03:08:38 +0100

    Mail,get dom,array mails allowed

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,10 @@ Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
 
 //*! - PRENDO GLI ELEMENTI DALLA DOM
 
+const playerNumber = document.getElementById('player-number');
+const cpuNumber = document.getElementById('cpu-number');
+const resultElement = document.getElementById('result');
+
 //!2 - GENERO DUE NUMERI RANDOMICI DA 1 A 6 PER PLAYER E PER COMPUTER
 
 const player = Math.floor(Math.random() * 6) + 1;
@@ -26,4 +30,24 @@ console.log(content);
 
 //!4 - STAMPO IN PAGINA I DUE NUMERI E IL RISULTATO
 
+playerNumber.innerHTML = 'Numero giocatore: ' + player;
+cpuNumber.innerHTML = 'Numero cpu: ' + cpu;
+resultElement.innerHTML = `<strong>${content}</strong>`;
 
+
+// - ESERCIZIO 2 MAIL Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.Mail Bonus Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+
+//?1 Prendo gli elementi dal DOM;
+
+const inputEmail = document.getElementById("Email");
+const button = document.getElementById("button");
+const message = document.getElementById("message");
+
+//? 2 - Crea un array con tre email autorizzate;
+const emailAuthorized = [
+    "Napoleon@gmail.com",
+    "Annibal@gmail.com",
+    "CharleMagne@gmail.com"
+]
+
+//?
